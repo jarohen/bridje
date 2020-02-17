f2bf715baa9f3b0776ee673f36efbc68fb5fba5e
James Henderson <james@jarohen.me.uk>
2020-02-17 21:40:07 +0000

    should probably make the Form stringrep something that isn't just the form again

--- language/src/test/kotlin/brj/reader/ReaderTest.kt
+++ language/src/test/kotlin/brj/reader/ReaderTest.kt
@@ -6,49 +6,53 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal fun Form.stringRep(): String = when (this) {
-    is BooleanForm -> bool.toString()
-    is StringForm -> string.replace(Regex("""[\\"\n\t\r]""")) {
-        when (it.value.first()) {
-            '\\' -> "\\\\"
-            '\n' -> "\\\n"
-            '\t' -> "\\\t"
-            '\r' -> "\\\r"
-            '\"' -> "\\\""
-            else -> it.value
+    is BooleanForm -> "(Bool $bool)"
+    is StringForm -> {
+        val s = string.replace(Regex("""[\\"\n\t\r]""")) {
+            when (it.value.first()) {
+                '\\' -> "\\\\"
+                '\n' -> "\\\n"
+                '\t' -> "\\\t"
+                '\r' -> "\\\r"
+                '\"' -> "\\\""
+                else -> it.value
+            }
         }
+        "\"$s\""
     }
-    is IntForm -> int.toString()
-    is BigIntForm -> "${bigInt}N"
-    is FloatForm -> float.toString()
-    is BigFloatForm -> "${bigFloat}M"
-    is SymbolForm -> sym.toString()
-    is QSymbolForm -> sym.toString()
-    is ListForm -> forms.map(Form::stringRep).joinToString(prefix = "(", separator = " ", postfix = ")")
-    is VectorForm -> forms.map(Form::stringRep).joinToString(prefix = "[", separator = " ", postfix = "]")
-    is SetForm -> forms.map(Form::stringRep).joinToString(prefix = "#{", separator = " ", postfix = "}")
-    is RecordForm -> forms.map(Form::stringRep).joinToString(prefix = "{", separator = " ", postfix = "}")
-    is QuotedSymbolForm -> "'${sym}"
-    is QuotedQSymbolForm -> "'${sym}"
-    is SyntaxQuotedSymbolForm -> "`${sym}"
-    is SyntaxQuotedQSymbolForm -> "`${sym}"
+    is IntForm -> "(Int $int)"
+    is BigIntForm -> "(BigInt $bigInt)"
+    is FloatForm -> "(Float $float)"
+    is BigFloatForm -> "(BigFloat $bigFloat)"
+    is SymbolForm -> "(Sym $sym)"
+    is QSymbolForm -> "(QSym $sym)"
+    is ListForm -> forms.joinToString(prefix = "(List ", transform = Form::stringRep, separator = " ", postfix = ")")
+    is VectorForm -> forms.joinToString(prefix = "(Vec ", transform = Form::stringRep, separator = " ", postfix = ")")
+    is SetForm -> forms.joinToString(prefix = "(Set ", transform = Form::stringRep, separator = " ", postfix = ")")
+    is RecordForm -> forms.joinToString(prefix = "(Rec ", transform = Form::stringRep, separator = " ", postfix = "}")
+    is QuotedSymbolForm -> "('Sym $sym)"
+    is QuotedQSymbolForm -> "('QSym $sym)"
+    is SyntaxQuotedSymbolForm -> "(`Sym $sym)"
+    is SyntaxQuotedQSymbolForm -> "(`QSym $sym)"
 }
 
 internal fun readForms(s: String) = readSourceForms(Source.newBuilder("brj", s, "<read-forms>").build())
+internal fun readFormsString(s: String) = readForms(s).joinToString(transform = Form::stringRep, separator = " ")
 
 internal class ReaderTest {
 
     @Test
     internal fun `test simple form`() {
         assertEquals(
-            listOf("[true foo]"),
-            readForms("[true foo]").map(Form::stringRep))
+            "(Vec (Bool true) (Sym foo))",
+            readFormsString("[true foo]"))
     }
 
     @Test
     internal fun `test quoting`() {
         assertEquals(
-            listOf("(:brj.forms/VectorForm [(:brj.forms/BooleanForm true) (:brj.forms/SymbolForm 'foo)])"),
-            readForms("'[true foo]").map(Form::stringRep))
+            "(List (QSym :brj.forms/VectorForm) (Vec (List (QSym :brj.forms/BooleanForm) (Bool true)) (List (QSym :brj.forms/SymbolForm) ('Sym foo))))",
+            readFormsString("'[true foo]"))
     }
 }
 
